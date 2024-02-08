8c32c08bacafda2c5ab6319880160f92b3cda5da
Chandan Vasu <chandanyadavparas@gmail.com>
2024-02-08 21:59:40 +0530

    header working fine

--- Template/Header-Template/header1.php
+++ Template/Header-Template/header1.php
@@ -4,9 +4,35 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php wp_head(); ?>
+    
 </head>
 <body <?php body_class(); ?>>
-    <header id="header1" style="background-color: <?php echo get_theme_mod('header_background_color_setting', '#ffffff'); ?>">
+    <div id="mySidenav" class="sidenav">
+        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+        <!-- Use WordPress menu here -->
+        <?php if ( has_custom_logo() ) : ?>
+                    <div class="site-logo"><?php the_custom_logo(); ?></div>
+                <?php else : ?>
+                    <?php if ( is_front_page() && is_home() ) : ?>
+                        <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+                    <?php else : ?>
+                        <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+                    <?php endif; ?>
+                <?php endif; ?>
+
+        <?php
+            wp_nav_menu(array(
+                'theme_location' => 'primary-menu',
+                'menu_id'        => 'primary-menu-id',
+                'container'      => false,
+                'menu_class'     => 'sidenav-menu'
+            ));
+        ?>
+        
+        
+    </div>
+
+    <header id="header1">
         <div class="header1-container">
             <div class="site-branding headerlogo headerlogo-1">
                 <?php if ( has_custom_logo() ) : ?>
@@ -20,17 +46,41 @@
                 <?php endif; ?>
                 <p class="site-description"><?php bloginfo( 'description' ); ?></p>
             </div><!-- .site-branding -->
+
+            
+
+            <!-- Add your navigation menu here if needed -->
             <nav id="site-navigation" class="main-navigation-header-1" role="navigation">
                 <?php
-                   wp_nav_menu(array(
+                    wp_nav_menu(array(
                        'theme_location' => 'primary-menu',
                        'menu_id'        => 'primary-menu-id',
                        'container'      => 'nav',
                        'container_class'=> 'primary-menu-container',
                        'menu_class'     => 'primary-menu-class'
-                   ));
+                   )); 
             
                 ?>
-            </nav><!-- #site-navigation -->
-        </div><!-- .container -->
-    </header><!-- #masthead -->
+            </nav>
+
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/paragraph.png" alt="" width="30px" height="30px" onclick="openNav()">
+
+
+            <!-- Add a button to open the side navigation menu -->
+            <!-- <span clsaa = 'threeline-vasu'style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776;</span> -->
+        </div><!-- .header1-container -->
+    </header><!-- #header1 -->
+
+    <?php wp_footer(); ?>
+    
+    <script>
+        function openNav() {
+            document.getElementById("mySidenav").style.width = "250px";
+        }
+
+        function closeNav() {
+            document.getElementById("mySidenav").style.width = "0";
+        }
+    </script>
+</body>
+</html>
