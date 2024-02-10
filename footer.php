d9a3a5214a1a1936cbfb4cefb6c3818dceef3eeb
Chandan Vasu <chandanyadavparas@gmail.com>
2024-02-10 14:35:05 +0530

    wp_footer(); add in fotter.php file , without wp_footer(); edit with elementor not show

--- footer.php
+++ footer.php
@@ -1,6 +1,9 @@
-<?php
+<?php wp_footer();
+
 $selected_footer_template = get_theme_mod('footer_template', 'footer1'); // Get the selected footer template from customizer
 
-// Include the selected footer template
+
 get_template_part('Template/Fotter/'. $selected_footer_template);
+
 ?>
+
